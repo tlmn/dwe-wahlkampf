5bfedbb4ed54f8d1fcdd98d57a32d85cd8ff4adb
tlmn <mail@tlmn.io>
2021-08-05 11:12:20 +0200

    remove console.log

--- src/components/atoms/number.js
+++ src/components/atoms/number.js
@@ -3,10 +3,11 @@ import React, { useRef, useState, useEffect } from "react"
 const Number = ({ children, className = "" }) => {
   const [width, setWidth] = useState(0)
   const ref = useRef(null)
+
   useEffect(() => {
     setWidth(ref.current.clientHeight)
-    console.log(width)
   }, [])
+  
   return (
     <h3
       className={`rounded-full ${className} font-bold text-4xl py-2 inline-block`}
