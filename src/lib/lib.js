b94af1b369a19f0b4866bf3e4b29f8bb14281a0a
tlmn <mail@tlmn.io>
2021-08-20 18:28:14 +0200

    add randomGenerator

--- src/lib/lib.js
+++ src/lib/lib.js
@@ -3,3 +3,7 @@ export const reverseCardColor = cardColor =>
 
 export const getRGB = cardColor =>
   cardColor === "yellow" ? `253, 216, 22` : `129, 28, 98`
+
+export const randomGenerator = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
