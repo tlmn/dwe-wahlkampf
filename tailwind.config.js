be8be32eaeddc686bd9cb038eee1e86dd2d0a63e
tlmn <mail@tlmn.io>
2021-08-20 18:28:24 +0200

    add media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
   darkMode: false,
   theme: {
     extend: {
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
       fontSize: {
         xs: ".75rem",
         sm: ".875rem",
