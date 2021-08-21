4a4afe6b8f9a340519cf1fe3780266c320554672
tlmn <mail@tlmn.io>
2021-08-21 13:28:44 +0200

    add text sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,8 @@ module.exports = {
         "5xl": "3rem",
         "6xl": "4rem",
         "7xl": "5rem",
+        "8xl": "6rem",
+        "9xl": "11rem",
       },
       spacing: {
         1: "8px",
