0a47ad0c3f472b7f2079bf27abbbd7739d2651fa
tlmn <mail@tlmn.io>
2021-09-10 09:52:15 +0200

    add turkish only and comment out other languages

--- src/data/lang.js
+++ src/data/lang.js
@@ -1,1 +1,7 @@
-export const langs = [{ code: "de", name: "deutsch" },{ code: "en", name: "english" }]
+export const langs = [
+  { code: "de", name: "deutsch" },
+  { code: "tr", name: "türkçe" },
+  /* { code: "en", name: "english" },
+  { code: "ru", name: "ру́сский" },
+  { code: "einfache-sprache", name: "einfache Sprache" }, */
+]
