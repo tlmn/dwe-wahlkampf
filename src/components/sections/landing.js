14bad76a697f4ec9868df3daa35cd5e947e4162c
tlmn <mail@tlmn.io>
2021-09-13 14:35:46 +0200

    update order on landing for turkish

--- src/components/sections/landing.js
+++ src/components/sections/landing.js
@@ -4,11 +4,14 @@ import LogoWordMark from "../../assets/svg/logoWordMark"
 import { Textfit } from "react-textfit"
 import LanguageSwitch from "../languageSwitch"
 import { FormattedHTMLMessage, useIntl } from "gatsby-plugin-intl"
+import useDataContext from "../../lib/useDataContext"
 
 const Landing = () => {
   const intl = useIntl()
   const ref = useRef(null)
   const [height, setHeight] = useState(0)
+  const { state } = useDataContext()
+  const { currentLocale } = state
   useEffect(() => setHeight(ref?.current?.clientHeight), [ref])
   return (
     <div className="bg-yellow text-purple font-bold">
@@ -29,26 +32,45 @@ const Landing = () => {
           <span className="block text-6xl md:text-7xl leading-none my-2">
             <FormattedHTMLMessage id="campaign.slogan" />
           </span>
-          <div className="w-full flex items-center justify-items-start my-2">
-            <div
-              style={{ height: `${height}px`, width: `${height}px` }}
-              className="mr-2 md:mr-4"
-            >
-              <Cross className="object-cover w-full h-full" />
+          {currentLocale !== "tr" && (
+            <div className="w-full flex items-center justify-items-start my-2">
+              <div
+                style={{ height: `${height}px`, width: `${height}px` }}
+                className="mr-2 md:mr-4"
+              >
+                <Cross className="object-cover w-full h-full" />
+              </div>
+              <span
+                className="text-8xl md:text-9xl block leading-none uppercase"
+                ref={ref}
+                dangerouslySetInnerHTML={{
+                  __html: intl.formatMessage({ id: "campaign.cta.yes" }),
+                }}
+              />
             </div>
-            <span
-              className="text-8xl md:text-9xl block leading-none uppercase"
-              ref={ref}
-              dangerouslySetInnerHTML={{
-                __html: intl.formatMessage({ id: "campaign.cta.yes" }),
-              }}
-            />
-          </div>
+          )}
           <div className="w-full">
             <Textfit mode="single" throttle={10}>
               {intl.formatMessage({ id: "campaign.cta.sub.long" })}
             </Textfit>
           </div>
+          {currentLocale === "tr" && (
+            <div className="w-full flex items-center justify-items-start my-2">
+              <div
+                style={{ height: `${height}px`, width: `${height}px` }}
+                className="mr-2 md:mr-4"
+              >
+                <Cross className="object-cover w-full h-full" />
+              </div>
+              <span
+                className="text-8xl md:text-9xl block leading-none uppercase"
+                ref={ref}
+                dangerouslySetInnerHTML={{
+                  __html: intl.formatMessage({ id: "campaign.cta.yes" }),
+                }}
+              />
+            </div>
+          )}
         </div>
       </div>
     </div>
