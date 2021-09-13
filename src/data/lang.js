fec9ec621182f54fe1cbdb8b13e94f39b0edeee4
tlmn <mail@tlmn.io>
2021-09-13 08:59:01 +0200

    add polish

--- src/data/lang.js
+++ src/data/lang.js
@@ -2,6 +2,7 @@ export const langs = [
   { code: "de", name: "deutsch" },
   { code: "tr", name: "türkçe" },
   { code: "en", name: "english" },
+  { code: "pl", name: "polski" },
   /*{ code: "ru", name: "ру́сский" },
   { code: "einfache-sprache", name: "einfache Sprache" }, */
 ]
