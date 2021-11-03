5a762890510c36eaa49f98c8bf727378d74de1e4
tlmn <mail@tlmn.io>
2021-11-03 15:30:09 +0100

    add landing

--- src/pages/letter.js
+++ src/pages/letter.js
@@ -2,10 +2,14 @@ import * as React from "react"
 import Layout from "../components/layout"
 import About from "../components/sections/about"
 import Letter from "../components/sections/letter"
+import Landing from "../components/sections/landing"
+import Seo from "../components/seo"
 
 const LetterPage = () => {
   return (
     <Layout>
+      <Seo />
+      <Landing />
       <About />
       <Letter />
     </Layout>
