cca0afb641df9cf99f70bfad0ee630e4ee49fa0c
tlmn <mail@tlmn.io>
2021-11-03 09:35:01 +0100

    remove unneeded import

--- src/pages/letter.js
+++ src/pages/letter.js
@@ -1,6 +1,5 @@
 import * as React from "react"
 import Layout from "../components/layout"
-import Landing from "../components/sections/landing"
 import About from "../components/sections/about"
 import Letter from "../components/sections/letter"
 
